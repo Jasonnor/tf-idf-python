238cdb442408c86312fe248d7d5b6e2f9501d40d
Jasonnor <wujason810@gmail.com>
2017-12-06 12:08:42 +0800

    Update weight

--- src/tf-idf-jieba.py
+++ src/tf-idf-jieba.py
@@ -1,9 +1,13 @@
 import jieba.analyse
 
-file_name = '../data/笑傲江湖.txt'
-topK = 50
+file_name = '../data/創世記/创世记01.txt'
+topK = 10
 content = open(file_name, 'rb').read()
 
-tags = jieba.analyse.extract_tags(content, topK=topK)
+print('tf-idf : ')
+for x, w in jieba.analyse.extract_tags(content, withWeight=True, topK=topK):
+    print('%s %s' % (x, w))
 
-print(", ".join(tags))
+# print('TextRank : ')
+# for x, w in jieba.analyse.textrank(content, withWeight=True, topK=topK):
+#     print('%s %s' % (x, w))
