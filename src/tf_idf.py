2202d6181362a23582993780cc2b3ae433d90cfc
Jasonnor <wujason810@gmail.com>
2017-12-06 19:39:19 +0800

    Update idf algorithm

--- src/tf_idf.py
+++ src/tf_idf.py
@@ -1,9 +1,10 @@
 import jieba
+from math import log
 from operator import itemgetter
 
 class tf_idf:
     def __init__(self):
-        self.files = []
+        self.files = {}
         self.corpus = {}
         self.stop_words = set(())
         content = open('./dictionary/stop_words.txt', 'rb').read().decode('utf-8')
@@ -21,7 +22,7 @@ class tf_idf:
             if len(w.strip()) < 2 or w.lower() in self.stop_words:
                 continue
             dictionary[w] = dictionary.get(w, 0.0) + 1.0
-            self.corpus[w] = self.corpus.get(w, 0.0) + 1.0
+            self.corpus[w] = 0.0
 
         # Get term frequency
         total = sum(dictionary.values())
@@ -29,24 +30,32 @@ class tf_idf:
             dictionary[k] /= total
 
         # Add tf to the corpus
-        self.files.append([file_name, dictionary])
+        self.files[file_name] = dictionary
 
-    def get_tf_idf(self, top_k):
+    def get_tf_idf(self, file_name, top_k):
+        # Get inverse document frequency
+        for w in self.corpus.keys():
+            w_in_f = 1.0
+            for dictionary in self.files:
+                if w in dictionary:
+                    w_in_f += 1.0
+            if w in self.files[file_name]:
+                self.corpus[w] = log(len(self.files) / w_in_f) * self.files[file_name][w]
         tags = sorted(self.corpus.items(), key=itemgetter(1), reverse=True)
         print(tags[:top_k])
 
     def similarities(self, list_of_words):
-        # building the query dictionary
+        # Building the query dictionary
         query_dict = {}
         for w in list_of_words:
             query_dict[w] = query_dict.get(w, 0.0) + 1.0
 
-        # normalizing the query
+        # Normalizing the query
         length = float(len(list_of_words))
         for k in query_dict:
             query_dict[k] = query_dict[k] / length
 
-        # computing the list of similarities
+        # Get the list of similarities
         sims = []
         for doc in self.files:
             score = 0.0
@@ -62,7 +71,8 @@ class tf_idf:
 if __name__ == "__main__":
     table = tf_idf()
 
-    file_name = '笑傲江湖/01.txt'
-    table.add_file(file_name)
+    for x in range(1, 10):#41
+        file_name = '笑傲江湖/' + str(x).zfill(2) + '.txt'
+        table.add_file(file_name)
 
-    table.get_tf_idf(top_k=20)
+    table.get_tf_idf(file_name='笑傲江湖/02.txt', top_k=20)
