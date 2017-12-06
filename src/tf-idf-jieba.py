864699696bed714a30d94d2a70a321c49226bb82
Jasonnor <wujason810@gmail.com>
2017-12-06 20:34:57 +0800

    Update stop words

--- src/tf-idf-jieba.py
+++ src/tf-idf-jieba.py
@@ -4,8 +4,7 @@ file_name = '../data/笑傲江湖/40.txt'
 topK = 10
 content = open(file_name, 'rb').read()
 
-# jieba.analyse.set_stop_words('./dictionary/stop_words.txt')
-# jieba.analyse.set_idf_path('./dictionary/idf.txt.big')
+jieba.analyse.set_stop_words('./dictionary/stop_words.txt')
 
 print('tf-idf : ')
 for x, w in jieba.analyse.extract_tags(content, withWeight=True, topK=topK):
