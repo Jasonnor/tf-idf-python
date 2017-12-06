82f72d422e701f5323f4cd46d9606963b853052d
Jasonnor <wujason810@gmail.com>
2017-12-06 18:18:50 +0800

    Add stop words

--- src/tf-idf-jieba.py
+++ src/tf-idf-jieba.py
@@ -1,8 +1,11 @@
 import jieba.analyse
 
-file_name = '../data/創世記/创世记01.txt'
+file_name = '../data/笑傲江湖/01.txt'
 topK = 10
 content = open(file_name, 'rb').read()
+
+# jieba.analyse.set_stop_words('./dictionary/stop_words.txt')
+# jieba.analyse.set_idf_path('./dictionary/idf.txt.big')
 
 print('tf-idf : ')
 for x, w in jieba.analyse.extract_tags(content, withWeight=True, topK=topK):
